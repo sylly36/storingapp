94605b8ad78917d9e8f9b8a280205b362b65f8ef
d294626 <117263785+d294626@users.noreply.github.com>
2023-03-03 10:59:40 +0100

    made insert query for meldingen

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -4,14 +4,25 @@
 $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit']; 
 $melder = $_POST['melder'];
+$type = 'test';
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+// echo $attractie . " / " . $capaciteit . " / " . $melder . " / " . $type;
 
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
+$query = "INSERT INTO meldingen (attractie, capaciteit, melder, type) 
+VALUES(:attractie, :capaciteit, :melder, :type)";
 
 //3. Prepare
+$statement = $conn->prepare($query);
 
 //4. Execute
+$statement->execute([
+    ":attractie"=> $attractie,
+    ":capaciteit" => $capaciteit,
+    ":melder" => $melder,
+    ":type" => $type
+
+]);
